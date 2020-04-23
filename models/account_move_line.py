6b8b3091fa99998d98620c0856d04d5c25191b8f
Alessio Medici <alessio.medici@clickode.it>
2020-04-23 12:22:17 +0200

    Modificato il tipo di campo.

--- models/account_move_line.py
+++ models/account_move_line.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    iban = fields.Char(related='invoice_id.res_partner_bank_id.acc_number')
+    iban = fields.Many2one(related='invoice_id.res_partner_bank_id')

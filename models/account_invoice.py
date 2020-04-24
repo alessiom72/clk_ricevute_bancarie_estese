1d1551b42af48ea16156e9a52b043844dd78d616
Alessio Medici <alessio.medici@clickode.it>
2020-04-24 11:46:50 +0200

    Corretto domain

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -1,18 +1,18 @@
 from odoo import fields, models, api, _
 
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    @api.onchange('partner_id')
-    def _get_bank_accounts(self):
-        if self.partner_id:
-            return {
-                'domain': {'res_partner_bank_id': [('partner_id', '=', self.partner_id.id)]}
-            }
+    def _get_partner_id(self):
+        return self.partner_id.id
 
-    res_partner_bank_id = fields.Many2one('res.partner.bank', string=_('RiBa Bank account'), readonly=True, states={'draft': [('readonly', False)]})
+    res_partner_bank_id = fields.Many2one('res.partner.bank', string=_('RiBa Bank account'), readonly=True,
+                                          states={'draft': [('readonly', False)]})
     riba = fields.Boolean(related='payment_term_id.riba')
-    res_partner_bank_name = fields.Char(related='res_partner_bank_id.bank_id.name', readonly=True, string=_('RiBa Bank name'))
+    res_partner_bank_name = fields.Char(related='res_partner_bank_id.bank_id.name', readonly=True,
+                                        string=_('RiBa Bank name'))
